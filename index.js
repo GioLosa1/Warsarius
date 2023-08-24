580aa03b7127a20279172e04de35712de51b02dd
George Losaberidze <127882097+GioLosa1@users.noreply.github.com>
2023-08-24 15:05:51 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
 
-
-$("a[href='#top']").click(function() {
-    $("html, body").animate({ scrollTop: 0 }, "slow");
-    return false;
-  });
-
-
+//scroll button
   let mybutton = document.getElementById("mytn");
 
-// When the user scrolls down 20px from the top of the document, show the button
+
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
       mybutton.style.display = "block";
     } else {
       mybutton.style.display = "none";
     }
   }
 
-// When the user clicks on the button, scroll to the top of the document
+
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
 }
+
+//scroll button //
+
+
+
